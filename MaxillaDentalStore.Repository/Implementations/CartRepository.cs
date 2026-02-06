83ce2ebbb98381215a00baed348ad57cf6badef4
jodx19 <elsafi19_dx72@yahoo.com>
2026-02-06 04:12:38 +0200

    Fix: Re-uploading all repositories completely

--- MaxillaDentalStore.Repository/Implementations/CartRepository.cs
+++ MaxillaDentalStore.Repository/Implementations/CartRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MaxillaDentalStore.Data;
+using MaxillaDentalStore.Data.Entities;
+using MaxillaDentalStore.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,41 @@ using System.Threading.Tasks;
 
 namespace MaxillaDentalStore.Repositories.Implementations
 {
-    public class CartRepository
+    public class CartRepository : ICartRepository
     {
+        private readonly AppDbContext _context;
+
+        public CartRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // get cart by user id with items and product details
+        public async Task<Cart?> GetCartByUserIdAsync(int userId)
+        {
+            return await _context.Carts
+                   .Include(c => c.CartItems)            
+                   .ThenInclude(ci => ci.Product)
+                   .FirstOrDefaultAsync(c => c.UserId == userId);
+        }
+
+        // add new cart for user
+        public async Task AddAsync(Cart cart)
+        {
+            await _context.Carts.AddAsync(cart);
+        }
+
+
+        // update cart (e.g., add/remove items, change quantities)
+        public void Update(Cart cart)
+        {
+            _context.Carts.Update(cart);
+        }
+
+        public void ClearCart(Cart cart)
+        {
+            
+            _context.Carts.Remove(cart);
+        }
     }
 }
